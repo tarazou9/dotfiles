8d50c7d603587e414161ed3f65847c220faa2f97
Ronnie Hicks <rohicks@lumeris.com>
2019-04-10 09:18:19 -0500

    Finalized adding zsh-nvm

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,17 +1,17 @@
-CUSTOM_DIR = ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+CUSTOM_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 
-echo "› re-installing powerlevel9k theme"
+echo "› re-installing powerlevel9k theme into $CUSTOM_DIR/themes/powerlevel9k"
 sudo rm -rf ${CUSTOM_DIR}/themes/powerlevel9k
 sudo git clone https://github.com/bhilburn/powerlevel9k.git ${CUSTOM_DIR}/themes/powerlevel9k
 
-echo "› re-installing zsh-autosuggestions"
+echo "› re-installing zsh-autosuggestions into ${CUSTOM_DIR}/plugins/zsh-autosuggestions"
 sudo rm -rf ${CUSTOM_DIR}/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${CUSTOM_DIR}/plugins/zsh-autosuggestions
 
-echo "› re-installing zsh-syntax-highlighting"
+echo "› re-installing zsh-syntax-highlighting into ${CUSTOM_DIR}/plugins/zsh-syntax-highlighting"
 sudo rm -rf ${CUSTOM_DIR}/plugins/zsh-syntax-highlighting
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${CUSTOM_DIR}/plugins/zsh-syntax-highlighting
 
-echo "› re-installing zsh-nvm"
+echo "› re-installing zsh-nvm into ${CUSTOM_DIR}/plugins/zsh-nvm"
 sudo rm -rf ${CUSTOM_DIR}/plugins/zsh-nvm
 sudo git clone https://github.com/lukechilds/zsh-nvm ${CUSTOM_DIR}/plugins/zsh-nvm
