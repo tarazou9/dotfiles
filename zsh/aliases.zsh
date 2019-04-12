2a3806fca4bd4cafdef1b002e747a8a25bc6058b
Ronnie Hicks <rohicks@lumeris.com>
2019-04-12 09:09:04 -0500

    Force sonar url on sonar-scanner so I don't have to specify it all the time

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias printcolors='for code ({000..255}) print -P -- "$code: %F{$code}This is how your text would look like%f"'
+
+alias sonar-scanner='sonar-scanner -Dsonar.host.url=$SONAR_URL'
